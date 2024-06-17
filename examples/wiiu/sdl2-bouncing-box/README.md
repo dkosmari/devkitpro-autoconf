daf6b1a0aa43acea461143d0a9012af8f927c841
Daniel K. O. (dkosmari) <none@none>
2024-06-17 13:19:47 -0300

    Wii U SDL2: added readme, sound effect, wuhb bundle.

---
+++ examples/wiiu/sdl2-bouncing-box/README.md
@@ -0,0 +1,45 @@
+# SDL2 Bouncing Box example for the Wii U
+
+This example shows how to build a SDL2 application for the Wii U. It also creates a "wuhb"
+bundle, with an audio file.
+
+
+## Build instructions
+
+1. `./bootstrap`
+
+2. `./configure --host=powerpc-eabi`
+
+3. `make`
+
+4. (Optional) `make run`
+
+
+## Explanation
+
+### `bootstrap`
+
+The [`bootstrap`](bootstrap) script makes sure the macros are available without needing
+them to be installed in the system.
+
+### `configure.ac`
+
+The main detail of [configure.ac](configure.ac) is calling `PKG_CHECK_MODULES` to look up
+both SDL2 and SDL2-mixer flags, using `pkg-config`.
+
+This time around, we are going to be using multiple recursive `Makefile.am`, so we don't
+need the `subdir-objects` option. We also use the recursive `run` target, so we can use
+`make run` in all Makefiles, using the `run-local` target.
+
+### `Makefile.am`
+
+In the top-level [`Makefile.am`](Makefile.am) we make a rule for building the `.wuhb`
+file. Both `all-local` and `run-local` recursive targets are used to build it.
+
+### `src/Makefile.am`
+
+In [`src/Makefile.am`](src/Makefile) we use both `SDL2_CFLAGS` and `SDL2_LIBS`, in
+addition to the `DEVKITPRO_*` variables.
+
+We also want to build the `.rpx` for both `all-local` and `run-local` targets.
+
--- examples/wiiu/sdl2-bouncing-box/src/main.cpp
+++ examples/wiiu/sdl2-bouncing-box/src/main.cpp
@@ -1,31 +1,38 @@
-#include <algorithm>
-#include <cstdlib>
-#include <vector>
-
 #include <SDL.h>
+#include <SDL_mixer.h>
 
 #include <whb/log.h>
 #include <whb/log_udp.h>
+#include <whb/file.h>
 
 
 int main()
 {
     SDL_Window* win = nullptr;
     SDL_Renderer* ren = nullptr;
+    Mix_Chunk* bonk = nullptr;
     int status = 0;
 
     constexpr int screen_width = 854;
     constexpr int screen_height = 480;
 
     WHBLogUdpInit();
-    std::atexit(reinterpret_cast<void (*)()>(WHBLogUdpDeinit));
 
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER | SDL_INIT_AUDIO) < 0) {
         WHBLogPrintf("Failed to init SDL: %s\n", SDL_GetError());
-        return -1;
+        status = -1;
+        goto error_final;
     }
-    std::atexit(SDL_Quit);
+
+    Mix_Init(0);
+
+    if (Mix_OpenAudio(48000, AUDIO_S16SYS, 2, 4096) == -1) {
+        WHBLogPrintf("Failed to open audio: %s\n", SDL_GetError());
+        status = -2;
+        goto error_quit;
+    }
+
 
     win = SDL_CreateWindow("Bouncing Box",
                            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -33,16 +40,23 @@ int main()
                            SDL_WINDOW_FULLSCREEN);
     if (!win) {
         WHBLogPrintf("Failed to create window: %s\n", SDL_GetError());
-        status = -2;
-        goto error_final;
+        status = -3;
+        goto error_audio;
     }
 
     ren = SDL_CreateRenderer(win, -1,
                              SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!ren) {
         WHBLogPrintf("Failed to create renderer: %s\n", SDL_GetError());
-        status = -3;
+        status = -4;
         goto error_win;
+    }
+
+    // Load asset from the .wuhb
+    bonk = Mix_LoadWAV("/vol/content/bonk.wav");
+    if (!bonk) {
+        WHBLogPrintf("error loading bonk.wav: %s\n", Mix_GetError());
+        goto error_bonk;
     }
 
 
@@ -52,8 +66,8 @@ int main()
         const float max_y = screen_height;
         float px = 0;
         float py = 0;
-        float vx = 1;
-        float vy = 1;
+        float vx = 3;
+        float vy = 3;
 
         SDL_Rect box{};
         box.w = size;
@@ -69,17 +83,49 @@ int main()
                 case SDL_QUIT:
                     running = false;
                     break;
+                case SDL_CONTROLLERBUTTONDOWN:
+                    switch (e.cbutton.button) {
+                    case SDL_CONTROLLER_BUTTON_B:
+                    case SDL_CONTROLLER_BUTTON_START:
+                        running = false;
+                        break;
+                    }
+                    break;
+                case SDL_CONTROLLERDEVICEADDED:
+                    {
+                        auto c = SDL_GameControllerOpen(e.cdevice.which);
+                        WHBLogPrintf("Added controller: %s\n",
+                                     SDL_GameControllerName(c));
+                    }
+                    break;
+                case SDL_CONTROLLERDEVICEREMOVED:
+                    {
+                        auto c = SDL_GameControllerFromInstanceID(e.cdevice.which);
+                        WHBLogPrintf("Removed controller: %s\n",
+                                     SDL_GameControllerName(c));
+                        SDL_GameControllerClose(c);
+                    }
+                    break;
                 }
             }
 
             px += vx;
             py += vy;
+            bool bounced = false;
             if ((px + size >= max_x && vx > 0)
-                || (px <= 0 && vx < 0))
+                || (px <= 0 && vx < 0)) {
                 vx = -vx;
+                bounced = true;
+            }
             if ((py + size >= max_y && vy > 0)
-                || (py <= 0 && vy < 0))
+                || (py <= 0 && vy < 0)) {
                 vy = -vy;
+                bounced = true;
+            }
+
+            if (bounced)
+                Mix_PlayChannel(-1, bonk, 0);
+
             // TODO: add some randomness to vx/vy after every bounce
 
             SDL_SetRenderDrawColor(ren, 0, 0, 40, 255);
@@ -88,17 +134,29 @@ int main()
             box.x = px;
             box.y = py;
             // TODO: randomly change box color after every bounce
-            SDL_SetRenderDrawColor(ren, 255, 100, 0, 255);
+            SDL_SetRenderDrawColor(ren, 255, 255, 0, 255);
             SDL_RenderFillRect(ren, &box);
 
             SDL_RenderPresent(ren);
         }
     }
 
+    WHBLogPrintf("Shutting down\n");
+
+    Mix_FreeChunk(bonk);
+ error_bonk:
+    WHBDeInitFileSystem();
+ error_fs:
     SDL_DestroyRenderer(ren);
  error_win:
     SDL_DestroyWindow(win);
+ error_audio:
+    Mix_CloseAudio();
+ error_quit:
+    Mix_Quit();
+    SDL_Quit();
  error_final:
+    WHBLogUdpDeinit();
 
     return status;
 }
